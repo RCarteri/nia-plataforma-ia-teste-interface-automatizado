a076b29c0ef9571947973ce8c8e519757289f5a0
C1324634 Ricardo Carteri Uehlein <c1324634@interno.bb.com.br>
2022-02-23 11:13:01 -0300

    fix: Delay para carregamento da página aumentado para 3s

--- src/main/java/utils/Razoes.java
+++ src/main/java/utils/Razoes.java
@@ -1,7 +1,7 @@
 package utils;
 
 public enum Razoes {
-	CARR_PAG("o carregamento da pagina", 2), CAP_TELA("a captura de tela", 1),
+	CARR_PAG("o carregamento da pagina", 3), CAP_TELA("a captura de tela", 1),
 	ENC_SEC("o encerramento desta sessão", 1), LOGIN("confirmação de login", 2),
 	TESTE_FTABB("=> FTABB - Testes Automatizados", 0), CARR_PLAT("carregamento da Plataforma", 25);
 

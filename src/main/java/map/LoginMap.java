f95e4610362f0d3b5799e6da23dc8260ab6b5509
C1324634 Ricardo Carteri Uehlein <c1324634@interno.bb.com.br>
2022-04-25 14:57:27 -0300

    feature()3.13.7: FindBy para Login

--- src/main/java/map/LoginMap.java
+++ src/main/java/map/LoginMap.java
@@ -1,19 +1,45 @@
 package map;
 
 import org.openqa.selenium.WebElement;
+import support.annotations.FindBy;
 
-import static support.GetElements.getElement;
+public class LoginMap extends BasePageObjects{
+//    public WebElement getInputUsername() {
+//        return getElement("#idToken1");
+//    }
+//
+//    public WebElement getInputPassword(){
+//        return getElement("#idToken2");
+//    }
+//
+//    public WebElement getBtnLogin(){
+//        return getElement("#loginButton_0");
+//    }
 
-public class LoginMap {
+    @FindBy(cssSelector = "#idToken1")
+    private WebElement inputUsername;
+
+    @FindBy(cssSelector = "#idToken2")
+    private WebElement inputPassword;
+
+    @FindBy(cssSelector = "#loginButton_0")
+    private WebElement btnLogin;
+
     public WebElement getInputUsername() {
-        return getElement("#idToken1");
+        if (inputUsername == null)
+            inputUsername = setElement("inputUsername");
+        return inputUsername;
     }
 
-    public WebElement getInputPassword(){
-        return getElement("#idToken2");
+    public WebElement getInputPassword() {
+        if (inputPassword == null)
+            inputPassword = setElement("inputPassword");
+        return inputPassword;
     }
 
-    public WebElement getBtnLogin(){
-        return getElement("#loginButton_0");
+    public WebElement getBtnLogin() {
+        if (btnLogin == null)
+            btnLogin = setElement("btnLogin");
+        return btnLogin;
     }
 }

c2e96689a4988b7239500f8119bda83625d1929b
C1324634 Ricardo Carteri Uehlein <c1324634@interno.bb.com.br>
2022-05-19 16:50:33 -0300

    feature(506847)5.0.0-SNAPSHOT: Add runner para ambientes de hlm e prd

---
+++ src/main/java/runners/all/TestCaseAllPrd.java
@@ -0,0 +1,32 @@
+package runners.all;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import runners.ConfRunner;
+
+import static support.APIRest.Ambiente.PROD;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		plugin = { "pretty", "io.qameta.allure.cucumber2jvm.AllureCucumber2Jvm" },
+		features = {"classpath:features/gestao", "classpath:features/primeiroAcesso"},
+		glue = "classpath:stepsDefinitions",
+		snippets = SnippetType.CAMELCASE,
+		tags = { "" },
+		monochrome = true
+)
+public class TestCaseAllPrd extends ConfRunner {
+	@BeforeClass
+	public static void init(){
+		confInit();
+	}
+
+	@AfterClass
+	public static void finish(){
+		confFinish(PROD);
+	}
+}

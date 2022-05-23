8f29efa7ff384356e1b31bda60646f12781fa72c
C1324634 Ricardo Carteri Uehlein <c1324634@interno.bb.com.br>
2022-05-23 10:00:01 -0300

    feature(506847)5.0.0-SNAPSHOT: Urls para o novo Allure nia-plat-ia-allure

--- src/main/java/support/APIRest/Dominio.java
+++ src/main/java/support/APIRest/Dominio.java
@@ -2,19 +2,19 @@ package support.APIRest;
 
 public enum Dominio {
     //Usado para evidenciar os testes cotidianos
-    DESENV("http://plataforma-ia-teste-interface-automatizado-allure-arq3.nia.desenv.bb.com.br/allure-docker-service", "servidor.allure.desenv"),
+    DESENV("http://plataforma-ia-teste-interface-automatizado-allure-arq3.nia.desenv.bb.com.br", "servidor.allure.desenv"),
 
     //Usado para evidenciar os testes na branch master
-    HML("http://plataforma-ia-teste-interface-automatizado-allure-arq3.nia.hm.bb.com.br/allure-docker-service", "servidor.allure.docker.ulr2"),
+    HML("http://plat-ia-allure.nia.hm.bb.com.br", "servidor.allure.hml"),
 
     //Usado para evidenciar os testes na entrega da Sprint
-    PROD("http://plataforma-ia-teste-interface-automatizado-allure-arq3.nia.prd.bb.com.br/allure-docker-service", "servidor.allure.docker.ulr3");
+    PROD("http://plat-ia-allure.nia.prd.bb.com.br", "servidor.allure.prd");
 
     private final String dominio;
     private final String servidor;
 
     Dominio(String dominio, String servidor) {
-        this.dominio = dominio;
+        this.dominio = dominio + "/allure-docker-service";
         this.servidor = servidor;
     }
 

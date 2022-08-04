a41a301e5a9f1485405e86f120235288915511b1
C1324634 Ricardo Carteri Uehlein <c1324634@interno.bb.com.br>
2022-08-04 13:29:55 -0300

    feature()6.0.1-SNAPSHOT: Ignorar cenários

--- src/main/java/stepsDefinitions/Hooks.java
+++ src/main/java/stepsDefinitions/Hooks.java
@@ -1,20 +1,30 @@
 package stepsDefinitions;
 
 import br.com.bb.ath.ftabb.exceptions.ElementoNaoLocalizadoException;
-import br.com.bb.ath.ftabb.gaw.Plataforma;
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import support.Utils;
 
+import static br.com.bb.ath.ftabb.gaw.Plataforma.fecharPaginaAtual;
+import static br.com.bb.ath.ftabb.gaw.Plataforma.recuperarTituloPagina;
+import static org.junit.Assume.assumeTrue;
 import static support.enums.Cookie.isLoggedIntranet;
 import static support.enums.SysProps.isLoggedPlataforma;
 
 public class Hooks {
+    @Before("@ignore")
+    public void skip_scenario(Scenario scenario){
+        System.out.println("Scenário ignorado: " + scenario.getName());
+        assumeTrue(false);
+    }
+
     @After
     public void tearDown() {
         try {
-            if (!isLoggedIntranet() & isLoggedPlataforma()) {
-                if (!Plataforma.recuperarTituloPagina().intern().equals("Plataforma BB | Analytics e Inteligência Artificial"))
-                    Plataforma.fecharPaginaAtual();
+            if (!isLoggedIntranet() && isLoggedPlataforma()) {
+                if (!recuperarTituloPagina().intern().equals("Plataforma BB | Analytics e Inteligência Artificial"))
+                    fecharPaginaAtual();
             }
         } catch (ElementoNaoLocalizadoException e) {
             new Utils().logError(e);

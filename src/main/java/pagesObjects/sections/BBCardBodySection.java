ead8e5e5b470c921a981621ddbcdbb8b74318bd4
C1324634 Ricardo Carteri Uehlein <c1324634@interno.bb.com.br>
2022-12-05 16:03:35 -0300

    refactor()7.16.0: Método getQuantResultados()

--- src/main/java/pagesObjects/sections/BBCardBodySection.java
+++ src/main/java/pagesObjects/sections/BBCardBodySection.java
@@ -2,7 +2,10 @@ package pagesObjects.sections;
 
 import map.BBCardBodyMap;
 
+import static org.junit.Assert.fail;
 import static support.Utils.getRandom;
+import static support.Utils.printLog;
+import static support.enums.LogTypes.ERROR;
 
 public class BBCardBodySection extends BBCardBodyMap {
     public String getDadoPesquisa(String dado) {
@@ -10,20 +13,24 @@ public class BBCardBodySection extends BBCardBodyMap {
             case "inválido":
                 return "#inválido";
             case "válido":
-                int indexCardRandom = getRandom(getListNomeComponente().size());
+                int indexCardRandom = getRandom(getQuantResultados());
                 return getListNomeComponente().get(indexCardRandom).getText();
             default:
                 throw new IllegalArgumentException("Os únicos dados aceitos são: válido e inválido");
         }
     }
 
+    private int getQuantResultados() {
+        if (getListNomeComponente().size() == 0) {
+            printLog("Não foi apresentado nenhum card na lista.", ERROR);
+            fail("Não foi apresentado nenhum card na lista.");
+        }
+        return getListNomeComponente().size();
+    }
+
     public boolean resultadosContemString(String palavraPesquisada) {
         return getListNomeComponente().stream()
                 .allMatch(nomeComponente -> nomeComponente.getText().equals(palavraPesquisada));
-    }
-
-    public int getQuantResultados() {
-        return getListNomeComponente().size();
     }
 
     public boolean siglasOk(String siglaSelecionada) {

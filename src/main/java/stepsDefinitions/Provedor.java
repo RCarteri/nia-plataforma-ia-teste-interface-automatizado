f85279c215382438a20d0d70ba4360549b95b61e
C1324634 Ricardo Carteri Uehlein <c1324634@interno.bb.com.br>
2022-12-19 14:40:38 -0300

    refactor()7.17.0: Página provedor

--- src/main/java/stepsDefinitions/Provedor.java
+++ src/main/java/stepsDefinitions/Provedor.java
@@ -7,21 +7,9 @@ import support.Utils;
 import static org.junit.Assert.assertTrue;
 
 public class Provedor extends Utils {
-//    @E("^acessar a página do provedor \"([^\"]*)\"$")
-//    public void acessarAPaginaDoProvedor(String provedor) {
-//        try {
-//            waitInvisibility(CIRCLE);
-//            cP.getMensagemAlerta("sucesso");
-//            cP.clickBtnFechar(false, "alerta");
-//            pP.acessarProvedor(provedor);
-//        } catch (Exception e) {
-//            logError(e);
-//        }
-//    }
-
     @Então("^deverá ser apresentados os provedores de serviços de cloud$")
     public void deveraSerApresentadosOsProvedoresDeServicosDeCloud() {
         capturaTela();
-        assertTrue(new ProvedorPage().getProvedores().size() > 0);
+        assertTrue(new ProvedorPage().getListProvedores().size() > 0);
     }
 }
